695a21ced471b198dd2ef485f937554aefac2d9c
Omar <omarbahgat0884@gmail.com>
2025-05-03 17:22:41 +0300

    initialized flask + user model

---
+++ backend/app/models/user.py
@@ -0,0 +1,16 @@
+from app import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = 'users'  # optional, default is class name in lowercase
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(20))
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<User {self.name}>"
